0de33fb440e86b7f296c6b60b257cfcc89360839
kamilnaja <kamilnaja@gmail.com>
2018-09-07 13:59:37 +0200

    initial commit

---
+++ src/main/java/com/strefatekstu/www/ncmsSpring/NcmsSpringApplication.java
@@ -0,0 +1,12 @@
+package com.strefatekstu.www.ncmsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcmsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcmsSpringApplication.class, args);
+	}
+}
