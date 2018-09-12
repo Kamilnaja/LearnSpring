7c43da041602696c944749a66bb3e5abbbb37cba
kamilnaja <kamilnaja@gmail.com>
2018-09-12 14:08:35 +0200

    added route for articles

--- src/main/java/com/strefatekstu/www/ncmsSpring/controllers/ArticleController.java
+++ src/main/java/com/strefatekstu/www/ncmsSpring/controllers/ArticleController.java
@@ -1,14 +1,23 @@
 package com.strefatekstu.www.ncmsSpring.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.strefatekstu.www.ncmsSpring.entites.Article;
+import com.strefatekstu.www.ncmsSpring.repositories.ArticleRepository;
+
 @RestController
 public class ArticleController {
 
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @GetMapping("/article")
-    public String getArticle(Model model) {
-        return "article";
+    public List<Article> getArticle(Model model) {
+        return articleRepository.findAll();
     }
 }
